8cfabca1d17e9a3fa93999a791dc9fe5a4336128
Johannes Frey <johannes.frey@loft.sh>
2024-08-05 15:06:50 +0200

    Also enable ksm during dev

--- cmd/vclusterctl/cmd/platform/add/cluster.go
+++ cmd/vclusterctl/cmd/platform/add/cluster.go
@@ -175,6 +175,7 @@ func (cmd *ClusterCmd) Run(ctx context.Context, args []string) error {
 			"--set", "agentOnly=true",
 			"--set", "prometheus.enabled=true",
 			"--set", "prometheus-opencost-exporter.enabled=true",
+			"--set", "kube-state-metrics.enabled=true",
 			"--set", "image=" + cmp.Or(os.Getenv("DEVELOPMENT_IMAGE"), "ghcr.io/loft-sh/enterprise:release-test"),
 		}
 	} else {
